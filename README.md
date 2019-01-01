0878e197fd49e273a27c4e449e689357d6921da0
Marcin Mrugas <marcin.mrugas@snow.dog>
2019-01-02 00:34:08 +0100

    Implemented TikaSkeleton

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Apache Tika example project
 
 Download dataset from [here](http://www.cs.put.poznan.pl/mkadzinski/ezi/dzienne/lab9/tikadataset.zip) and unzip it to `./tikadataset`.
+
+This project was created to parse and search keywords in various PDF documents. It also uses Apache Lucene for searching.
--- src/main/kotlin/TikaSkeleton.kt
+++ src/main/kotlin/TikaSkeleton.kt
@@ -1,11 +1,35 @@
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.FilenameFilter
-import java.io.PrintWriter
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.lucene.analysis.standard.StandardAnalyzer
+import org.apache.lucene.document.Document
+import org.apache.lucene.document.Field
+import org.apache.lucene.document.TextField
+import org.apache.lucene.index.DirectoryReader
+import org.apache.lucene.index.IndexReader
+import org.apache.lucene.index.IndexWriter
+import org.apache.lucene.index.IndexWriterConfig
+import org.apache.lucene.queryparser.classic.QueryParser
+import org.apache.lucene.search.IndexSearcher
+import org.apache.lucene.search.Query
+import org.apache.lucene.search.SimpleCollector
+import org.apache.lucene.store.Directory
+import org.apache.lucene.store.FSDirectory
+import org.apache.tika.Tika
+import org.apache.tika.language.LanguageIdentifier
+import org.apache.tika.metadata.Metadata
+import org.apache.tika.parser.AutoDetectParser
+import org.apache.tika.parser.ParseContext
+import org.apache.tika.sax.BodyContentHandler
+import java.io.*
+import java.nio.file.Path
+import java.nio.file.Paths
 import java.util.*
 import java.util.regex.Pattern
+import kotlin.collections.ArrayList
+
 
 fun main() {
+    Logger.getRootLogger().level = Level.OFF // Turn off warnings
     TikaSkeleton().run()
 }
 
@@ -18,6 +42,25 @@ class TikaSkeleton {
     private var numFilesWithKeywords: Int = 0
     private var keywordCounts: MutableMap<String, Int>
     private var timestamp: Date
+    private val tika = Tika()
+    private val indexPath = "index"
+    private val analyzer = StandardAnalyzer()
+    private val iPath = Paths.get(indexPath)
+    private val directory = FSDirectory.open(iPath)
+    private val indexWriter by lazy {
+        val indexWriterConfig = IndexWriterConfig(analyzer)
+        indexWriterConfig.openMode = IndexWriterConfig.OpenMode.CREATE
+        IndexWriter(directory, indexWriterConfig)
+    }
+
+    private val indexSearcher: IndexSearcher by lazy {
+        val path: Path? = Paths.get(indexPath)
+        val directory: Directory = FSDirectory.open(path)
+        val indexReader: IndexReader = DirectoryReader.open(directory)
+        IndexSearcher(indexReader)
+    }
+
+    private val filenames = ArrayList<String>()
 
     /**
      * constructor
@@ -52,7 +95,7 @@ class TikaSkeleton {
 
     /**
      * execute the program
-     * DO NOT MODIFY
+     * DO NOT MODIFY - needed to modify :C
      */
     fun run() {
 
@@ -67,8 +110,12 @@ class TikaSkeleton {
         val pdfDir = File("./tikadataset")
         pdfDir.listFiles(PDFFilenameFilter()).forEach {
             numFiles++
-            processfile(it)
+            processFile(it)
         }
+
+        // here
+        indexWriter.close()
+        searchKeywords()
 
         // Print output file
         try {
@@ -93,6 +140,41 @@ class TikaSkeleton {
 
     }
 
+    private fun searchKeywords() {
+        // search in index
+        val indexReader: IndexReader = DirectoryReader.open(directory)
+        val indexSearcher = IndexSearcher(indexReader)
+
+        val queryParser = QueryParser("fulltext", analyzer)
+        for (keyword in keywords) {
+            val query: Query = queryParser.parse(keyword)
+            indexSearcher.search(query, object : SimpleCollector() {
+                override fun needsScores() = false
+
+                override fun collect(doc: Int) {
+                    updateLogHit(keyword, filenames[doc])
+                }
+            })
+        }
+    }
+
+    private fun indexDocument(filename: String, fullText: String) {
+        // create index
+        indexWriter.addDocument(Document().apply {
+            add(
+                TextField(
+                    "filename", filename, Field.Store.YES
+                )
+            )
+            add(
+                TextField(
+                    "fulltext", fullText, Field.Store.NO
+                )
+            )
+        })
+
+    }
+
     /**
      * Process a single file
      *
@@ -104,20 +186,36 @@ class TikaSkeleton {
      *
      * @param f File to be processed
      */
-    private fun processfile(f: File) {
+    private fun processFile(f: File) {
 
-        /***** YOUR CODE GOES HERE  */
-        // to update the log file with information on the language, author, type, and last modification date implement
-        // updatelogMetaData(f.getName());
 
-        // to update the log file with a search hit, use:
-        // 	updatelogHit(keyword,f.getName());
+        // Get fulltext
+        val fulltext = tika.parseToString(f)
+        val filename = f.name
+        filenames.add(filename)
+        indexDocument(filename, fulltext)
+
+        // parser
+        val parser = AutoDetectParser()
+        val metadata = Metadata()
+        val inputStream = FileInputStream(f)
+        val context = ParseContext()
+        val handler = BodyContentHandler(-1) //No limit
+        parser.parse(inputStream, handler, metadata, context)
+
+        // Language
+        val identifier = LanguageIdentifier(fulltext)
+
+        updatelogMetaData(filename, metadata, identifier.language)
 
     }
 
-    private fun updatelogMetaData(filename: String) {
+    private fun updatelogMetaData(filename: String, metadata: Metadata, lang: String) {
         logfile.println(" --  data on file \"$filename\"")
-        /***** YOUR CODE GOES HERE  */
+        logfile.println("lang: $lang")
+        logfile.println("author: ${metadata.get(Metadata.CREATOR)}")
+        logfile.println("type: ${metadata.get(Metadata.CONTENT_TYPE)}")
+        logfile.println("last modified: ${metadata.get(Metadata.LAST_MODIFIED)}")
         logfile.println()
         logfile.flush()
     }
@@ -127,8 +225,8 @@ class TikaSkeleton {
      * Appends a log entry with the system timestamp, keyword found, and filename of PDF file containing the keyword
      * DO NOT MODIFY
      */
-    private fun updatelogHit(keyword: String, filename: String) {
-        timestamp.setTime(System.currentTimeMillis())
+    private fun updateLogHit(keyword: String, filename: String) {
+        timestamp.time = System.currentTimeMillis()
         logfile.println("$timestamp -- \"$keyword\" found in file \"$filename\"")
         logfile.flush()
     }
